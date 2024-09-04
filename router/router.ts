a564f20fd2b375c70d3e7127dedec720e52976f9
Matteo Monterosso <matteo.monterosso@oclock.school>
2024-09-04 11:11:37 +0200

    cw

--- router/router.ts
+++ router/router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import * as animalControler from '../controllers/animalControler';
+import { controllerWrapper as cw } from '../utils/controllerWrapper';
 
 export const router = Router();
 
 // Routes des Animaux
-router.get('/animals', animalControler.getAllAnimals);
+router.get('/animals', cw(animalControler.getAllAnimals));
