2c89f298dbce791a9ce892927e6d6f6dc247236a
Matteo Monterosso <matteo.monterosso@oclock.school>
2024-09-10 15:39:08 +0200

    Amélioration getAllAnimals pour creer le Owner dans la foulée

--- index.ts
+++ index.ts
@@ -16,17 +16,19 @@ import { router } from './router/router';
 app.use(express.urlencoded({ extended: true })); // Parser les bodies de type "application/www-form-urlencoded"
 app.use(express.json()); // Parser les bodies de type "application/json"
 
-// // Utiliser une clé secrète pour signer les cookies
-// app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+// Utiliser une clé secrète pour signer les cookies
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
 
-// // Utiliser les cookies de manières sécurisée
-// app.use(session({
-//   saveUninitialized: true,
-//   resave: true,
-//   secret: process.env.SESSION_SECRET as string,  // Empêche la falsification des sessions
-//   // Empêche le JS du client d'accéder aux cookies
-//   cookie: { secure: true, httpOnly: true, maxAge: 10800000 }, // Le navigateur envoie le coookie que sur HTTPS
-// }));
+// Utiliser les cookies de manières sécurisée
+app.use(
+  session({
+    saveUninitialized: true,
+    resave: true,
+    secret: process.env.SESSION_SECRET as string, // Empêche la falsification des sessions
+    // Empêche le JS du client d'accéder aux cookies
+    cookie: { secure: true, httpOnly: true, maxAge: 10800000 }, // Le navigateur envoie le coookie que sur HTTPS
+  })
+);
 
 // Authorize CORS requests
 app.use(cors('*' as any)); // * = tous les domaines (pour nous faciliter la vie sur la saison future, mais en pratique, on devrait limiter l'accès à notre API uniquement au front qui va nous appeler !)
